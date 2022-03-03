945fc47aeae36685f9addf3fc1a084336ae2040a
stronex <pz@stronex.pl>
2022-03-03 15:20:12 +0100

    Update pierogi-ukrainskie.include.js

--- pierogi-ukrainskie.include.js
+++ pierogi-ukrainskie.include.js
@@ -1,15 +1,20 @@
-﻿/* 
- * WSPIERAMY WALCZACA UKRAINE - PIEROGI UKRAINSKIE nie ruskie!
+/* 
+ * WSPIERAMY WALCZACA UKRAINE - PIEROGI UKRAINSKIE nie ruskie
+ * Symboliczny sprzeciw wobec wojny na Ukrainie
+ * Wprawdzie pierogi ruskie pochodzą z Rusi Czerwonej, nie Rosji,
+ * mimo to, manifestując sprzeciw wobec wojny,
+ * symbolicznie sprzedajemy je jako UKRAIŃSKIE! 
  * 
  * https://restauracja.online
  * System zamawiania jedzenia na stronie restauracji
  * 
  * W celu automatycznej zmiany tekstu Pierogi ruskie na Pierogi UKRAIŃSKIE z przekreślonym ruskie
  * Dodaj skrypt w sekcji head lub przed elementem </body> (na samym końcu kodu strony)
- * <script type="text/javascript" id="pierogiRuskie" src="https://restauracja.online/assets/js/pierogi-ukrainskie.include.js" data-add-flag-to-logo=false new-name="UKRAIŃSKIE" ukraine-flag=true description-ukrainskie="'Wspieramy walczącą UKRAINĘ!" description-ruskie="Symboliczny sprzeciw wobec wojny, mimo że to pierogi ruskie nie rosyjskie" ></script>
+ * <script type="text/javascript" id="pierogiRuskie" src="https://restauracja.online/assets/js/pierogi-ukrainskie.include.js" data-add-flag-to-logo=false new-name="UKRAIŃSKIE"     ukraine-flag=true description-ukrainskie="'Wspieramy walczącą UKRAINĘ!" description-ruskie="Symboliczny sprzeciw wobec wojny, mimo że to pierogi ruskie nie rosyjskie" ></script>
  * 
  * Licencja MIT (Kopiuj i uzywaj bez ograniczen rowniez komercyjnie)
  * Autor: Stronex.pl producent platformy zamawiania restauracja.online oraz zamawiaj.online
+ * Pytania, pomoc w instalacji dla restauracji (pomoc gratis): pz@stronex.pl
  * 
  */
 
